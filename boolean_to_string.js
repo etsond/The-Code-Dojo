136abcee0ec13cdccbb7fff1d7598fcd06782cf0
Etson <etsond@ufl.edu>
2023-03-10 20:58:39 -0500

    boolean to string yes or no

---
+++ boolean_to_string.js
@@ -0,0 +1,11 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord(bool){
+    // checking if bool is true
+    if(bool){
+        return "Yes"
+        // if not return a string No
+    }else{
+        return "No"
+    }
+}

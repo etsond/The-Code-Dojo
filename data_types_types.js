ea0f3eedeb6518843edce242c8d37fd0aa829c02
Etson <etsond@ufl.edu>
2023-03-20 14:42:04 -0400

    data types

---
+++ data_types_types.js
@@ -0,0 +1,24 @@
+function getLength(arr){
+  //return length of arr
+  return arr.length
+}
+function getFirst(arr){
+  //return the first element of arr
+  return arr[0]
+}
+function getLast(arr){
+  //return the last element of arr
+  let last = arr[arr.length - 1]
+  return last
+}
+function pushElement(arr){
+  var el=1;
+  //push el to arr
+    arr.push(el) 
+  return arr
+}
+function popElement(arr){
+  //pop an element from arr
+   arr.pop()
+  return arr
+}

65c0c8c40ad9547ed741d14b9d6ca0b6a9a0e02b
Etson <etsond@ufl.edu>
2023-03-25 15:57:06 -0400

    calculate bmi

---
+++ bmi_Calc.js
@@ -0,0 +1,23 @@
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+
+function bmi(weight, height) {
+  let bmiP = weight / (height * height)
+  if(bmiP <= 18.5 ){
+    return "Underweight"
+  }else if(bmiP <= 25.0){
+    return "Normal"
+  }else if(bmiP <= 30.0){
+    return "Overweight"
+  }else{
+    return "Obese"
+  }
+}

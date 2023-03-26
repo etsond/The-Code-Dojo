7466b9a8f4d75f909418be2e61627c1b44d0a9b4
Etson <etsond@ufl.edu>
2023-03-26 07:40:41 -0400

    found needle

---
+++ needle_in_haystack.js
@@ -0,0 +1,18 @@
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+
+
+
+
+function findNeedle(haystack) {
+  
+  const index = haystack.findIndex(item => item === "needle");
+  
+  return `found the needle at position ${index}`;
+}

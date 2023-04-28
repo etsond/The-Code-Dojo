1910ee07e086192f5c88abd5098a6629def9d07d
Etson <etsond@ufl.edu>
2023-04-27 21:10:18 -0400

    sorting

---
+++ sort_odd.js
@@ -0,0 +1,18 @@
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+function sortArray(array) {
+  // get all the odd numbers & sort them
+  const odds = array.filter(num => num % 2 !== 0).sort((a, b) => a - b);
+  
+  // Replace odd nums with sorted nums 
+  return array.map(num => {
+    if (num % 2 !== 0) {
+      return odds.shift();
+    }
+    return num;
+  });
+}

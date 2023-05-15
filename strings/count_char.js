249db0c8aa3b7df574a64e776c7fad680c45d68d
Etson <etsond@ufl.edu>
2023-05-15 10:48:24 -0400

    count your character

---
+++ strings/count_char.js
@@ -0,0 +1,16 @@
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(string) {
+let result = {};
+for (let i = 0; i < string.length; i++) {
+let char = string[i];
+if (result[char]) {
+result[char]++;
+} else {
+result[char] = 1;
+}
+}
+return result;
+}
